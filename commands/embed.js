1b80d485c536c2a757051d4f0efe5003bb914391
AstaGotBandzz <79398687+AstaGotBandzz@users.noreply.github.com>
2021-03-03 13:30:02 -0500

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -4,14 +4,14 @@ module.exports = {
     execute(message, args, Discord) {
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#990000')
-        .setTitle('Nekoma Bot Commands')
+        .setTitle('Arkham Bot Commands')
         .addFields(
-            {name:'N!help', value: 'Lists all the avaliable commands'},
-            {name:'N!ban', value: 'Bans user from the server'},
-            {name:'N!mute', value: 'Mutes a user from chat'},
-            {name: 'N!clear', value: `Deletes messages from a given channel(1-100)`},
-            {name: 'N!Kick', value: 'Kicks a user from the server'},
-            {name: 'N!unmute', value: 'Unmutes a user from chat'},
+            {name:'A!help', value: 'Lists all the avaliable commands'},
+            {name:'A!ban', value: 'Bans user from the server'},
+            {name:'A!mute', value: 'Mutes a user from chat'},
+            {name: 'A!clear', value: `Deletes messages from a given channel(1-100)`},
+            {name: 'a!Kick', value: 'Kicks a user from the server'},
+            {name: 'A!unmute', value: 'Unmutes a user from chat'},
         )            
         .setImage('https://media.discordapp.net/attachments/786578219092410419/809175852276908062/ezgif.com-gif-maker_46.gif')
         .setFooter('Custom Bot | Coded by !Asta#0001');
